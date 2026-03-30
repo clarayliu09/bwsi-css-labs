f8d840c8b6299fabba563542946bd690bd8e52bb
clara <claraliu990@gmail.com>
2026-03-29 22:21:02 -0400

    add introduction to lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -12,5 +12,9 @@ def main():
 
     print(f"{name}, Welcome to the CSS course!")
 
+    print("Hi everyone! I'm Clara, and I'm a sophomore from Massachusetts. I'm mainly into physics olympiads and competitive programming, but I also do math competitions and CAD every now and then. I'm also on my school's Science Olympiad team, like many of you.")
+    print("In my free time, I enjoy oil painting, playing the piano, and vibe coding random projects. I also recently took an interest in computer graphics, so please feel free to share knowledge!")
+    print("I'm excited to learn more about quadrotors and UAV with you all!")
+
 if __name__ == "__main__":
     main()
